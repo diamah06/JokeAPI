ca052277219f140a704a9eefd4ef3abecfd25b2c
diamah06 <tabalmahdia@yahoo.fr>
2023-03-30 14:09:20 +0200

    Initial Commit

---
+++ JokeAPI/JokeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeAPIApp.swift
+//  JokeAPI
+//
+//  Created by Mahdia Amriou on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct JokeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
